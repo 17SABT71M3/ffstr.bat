7cac2a985c8b0b08d5860004fe147a92c9fec410
Christian Programmer "Dr. Dream$ <117502862+175M3H3RE@users.noreply.github.com>
2024-02-29 15:54:42 +0530

    Add files via upload

--- extract.bat
+++ extract.bat
@@ -4,7 +4,9 @@ set bemindfulof=%~1
 set regex=%2
 set regex=%regex:[Capital]=[A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z]%
 set string=%3
-set demeaner=%4
+set string=%string:|=^|%
+ReM if "%4" NEQ "" echo %4|findstr /r "^/[0-9][0-9]*$"
+REM echo Delimiter = "%bemindfulof% "
 REM echo demeaner=x%demeaner%x
 REM echo regex = %regex%
 REM echo string= %string%
@@ -22,17 +24,15 @@ rem echo:It's time for some token frenzy..
 set whoami=%%i
 set /a token=1
 set /a continue=0
-set endstring=" "
-set /a taken=1
 :loop
+set /a uploadtaken=1
 set /a found=0
 set upload=
-for /f "tokens=%token% delims=%bemindfulof% " %%i in (%string%) do set /a found=1&echo %%i|findstr /r %regex% >NUL&&(if %taken% NEQ 0 set upload="%%i")
+for /f "tokens=%token% delims=%bemindfulof% " %%i in (%string%) do set /a found=1&echo %%i|findstr /r %regex% >NUL&&(set upload="%%i"&set /a uploadtaken=0)
 :continue
-REM echo if "%upload%" NEQ "" if %taken% NEQ 0 
-if "%upload%" NEQ "" if %taken% NEQ 0 set /a taken=0&set finalupload=%upload%& REM echo HAHA _
-if %found%==0 goto :end
+if %uploadtaken%==0 echo %upload%
+if %found%==0 goto :eof
 set /a token+=1
 goto :loop
 :end
-echo %finalupload%
+echo %upload%
