a0e5e26967ea3df50ce7e163e82b9d5bc153a108
Christian. )C( <117502862+17SABT71M3@users.noreply.github.com>
2024-03-03 01:29:37 +0530

    Add files via upload

--- extract.bat
+++ extract.bat
@@ -1,18 +1,69 @@
 @echo off
 set finalupload=
-set bemindfulof=%~1
+set bemindfulof=%~2
  REM <WEL COME> /\Namastey0`:,
-set regex=%2
+set handle=%~1
+
+
+set regex=%3
   set regex=%regex:[Capital]=[ABCDEFGHIJKLMNOPQRSTUVWXYZ]%
 set regex=%regex:[^Capital]=[^ABCDEFGHIJKLMNOPQRSTUVWXYZ]%
   set regex=%regex:[^LOWER]=[^abcdefghijklmnopqrstuvwxyz]%
     set regex=%regex:[LOWER]=[abcdefghijklmnopqrstuvwxyz]%
-set string=%3
+
+if /i "%handle%"=="/c:" goto skip_handle_init
+setlocal enabledelayedexpansion
+set thirst=!handle!
+set first=!thirst:~0,1!
+if "!first!" NEQ "/" goto :printhelpmenu
+set local=
+for /l %%i in (1,1,5) do set something=!thirst:~%%i,1!&set local=!local! !something!
+for %%i in (%local%) do (
+set /a cool=0
+
+if /i "&%%i&"=="&b&" set /a cool=1
+if /i "&%%i&"=="&e&" set /a cool=1
+if /i "&%%i&"=="&l&" set /a cool=1
+if /i "&%%i&"=="&r&" set /a cool=1
+if /i "&%%i&"=="&i&" set /a cool=1
+if /i "&%%i&"=="&x&" set /a cool=1
+if /i "&%%i&"=="&v&" set /a cool=1
+if !cool!==0 goto :printhelpmenu
+)
+goto _init
+endlocal disabledelayedexpansion
+:printhelpmenu
+echo:The following are valid FINDSTR handles in the context of this
+echo:batch script:
+echo:  /C:        Uses specified string as a literal search string.
+echo:  /B         Matches pattern if at the beginning of a line.
+echo:  /E         Matches pattern if at the end of a line.
+echo:  /L         Uses search strings literally.
+echo:  /R         Uses search strings as regular expressions.
+echo:  /S         Searches for matching files in the current directory and all
+echo:             subdirectories.
+echo:  /I         Specifies that the search is not to be case-sensitive.
+echo:  /X         Prints lines that match exactly.
+echo:  /V         Prints only lines that do not contain a match.
+echo:
+echo:All above handles must be combined if using more than one.
+echo:Eg. /bel OR /RI
+echo:
+echo:Not----^> /b /e /l OR /R /I
+goto :eof
+:_init
+
+REM caution space sensitive code
+set handle=%handle% 
+:skip_handle_init
+set string=%4
 set string=%string:^=^^%
 set string=%string:|=^|%
 set string=%string:&=^&%
 set string=%string:>=^>%
 set string=%string:<=^<%
+
+
 
 ReM (((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((
 
@@ -39,11 +90,11 @@ set /a uploadtaken=1
 set /a found=0
 set upload=
 
-for /f "tokens=%token% delims=%bemindfulof%" %%i in (%string%) do set /a found=1&echo %%i|findstr /r %regex% >NUL&&(set upload="%%i"&set /a uploadtaken=0)
+for /f "tokens=%token% delims=%bemindfulof%" %%i in (%string%) do set /a found=1&echo %%i|findstr %handle%%regex% >NUL&&(set upload="%%i"&set /a uploadtaken=0)
 if %uploadtaken% NEQ 1 goto continue
-for /f "tokens=%token% delims=%bemindfulof%" %%i in (%string%) do set /a found=1&echo "%%i"|findstr /r %regex% >NUL&&(set upload="%%i"&set /a uploadtaken=0)
+for /f "tokens=%token% delims=%bemindfulof%" %%i in (%string%) do set /a found=1&echo "%%i"|findstr %handle%%regex% >NUL&&(set upload="%%i"&set /a uploadtaken=0)
 :continue
-if %uploadtaken%==0 for /f "tokens=*" %%i in (%upload%) DO for /f "delims=" %%a in ('echo:%%i') do echo %%a
+if %uploadtaken%==0 for /f "delims=" %%i in (%upload%) DO for /f "delims=" %%a in ('echo:%%i') do echo %%a
 if %found%==0  goto :eof
 set /a token+=1
 goto :loop
